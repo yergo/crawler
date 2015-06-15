87bf428c49761fed23a853763affb04b5d548be8
yergo <yergo@yergo-MSI>
2015-06-15 17:44:00 +0200

    Revert "threaded simulation working concept"
    This reverts commit 54540c894b44949c64c6b32316f56a3a8c7851f2.

--- app/tasks/TrojmiastoTask.php
+++ app/tasks/TrojmiastoTask.php
@@ -3,100 +3,35 @@
 class TrojmiastoTask extends \Phalcon\CLI\Task
 {
 
-	private static $processes;
-
 	public function mainAction()
 	{
 
-		// ./console trojmiasto <<< "{test:\"test\"}"
-
-
 		$filename = "http://ogloszenia.trojmiasto.pl/nieruchomosci-sprzedam/?formSended=1&formSendedFrom=advSearchLeft&searchFormSended=1&id_kat=101&katlist=1&id_kat_list%5B101%5D=101&cena_min=&cena_max=&rodzaj_nieruchomosci=100&cenam2_min=&cenam2_max=&powierzchnia_min=35&powierzchnia_max=&adres_ulica_i_nr=&districtListWhatSelected=Gda%F1sk+Wrzeszcz+G%F3rny&f1i%5B0%5D=&e1i=139&f1i=&l_pokoi_min=&l_pokoi_max=&pietro_min=&pietro_max=&l_pieter_min=&l_pieter_max=&rok_budowy_min=&rok_budowy_max=&powierzchnia_dzialki_min=&powierzchnia_dzialki_max=&typ_ogrzewania=&slowa_option=all_phrases&slowa=&obList=&data_wprow=all&order=data_wazne_SMS+DESC%2C+data_wprow+DESC&limit=100&id_kat=&limit=100&id_kat=101";
-
-		/**
-		 * @todo iterator
-		 */
+		
+		print('Gathering result list for Trojmiasto' . PHP_EOL);
 		$advertisements = new \Application\Models\Services\Trojmiasto\ResultsList($filename);
-		print('Downloaded...' . PHP_EOL);
-//		$x = 0;
 		
-
-		$processes = [];
-		foreach ($advertisements->urls as $id => $url) {
-
-			$input = [
-				'source_name' => $advertisements->source_name,
-				'source_id' => $id,
-				'url' => $url
-			];
-
-			$input = json_encode($input);
-
-			$processes[] = $this->proceedContent($input);
+		print('Building threads' . PHP_EOL);
+		$stack = [];
+		
+		foreach($advertisements->urls as $key => $url) {
 			
-//			if($x++ > 3) {
-//				break;
-//			}
-		}
-
-		return $this->killProcesses($processes);
-	}
-
-	public function clusterAction()
-	{
-		$json = json_decode(file_get_contents('php://stdin'));
-		print('downloading' . PHP_EOL);
-		sleep(1);
-
-		print('parsed' . PHP_EOL);
-
-//		var_dump($json);
-		print('saved' . PHP_EOL);
-		
-		exit(0);
-	}
-
-	private function proceedContent($input)
-	{
-		$descriptors = [
-				0 => array("pipe", "r"), // stdin is a pipe that the child will read from
-				2 => array("file", "error-output.txt", "a") // stderr is a file to write to
-			];
-		
-		$process = proc_open('./console trojmiasto cluster', $descriptors, $pipes);
-
-		if (is_resource($process)) {
-			fwrite($pipes[0], $input);
-			fclose($pipes[0]);
-		}
-
-		return $process;
-	}
-
-	private function killProcesses($processes)
-	{
-		$killed = 0;
-		
-		while(count($processes) > 0) {
-			$unset = [];
-			foreach ($processes as $k => $process) {
-				$status = proc_get_status($process);
-				if(!$status['running']) {
-					proc_close($process);
-					$killed++;
-					$unset[] = $k;
-					print('finished ' . $killed . PHP_EOL);
-				}
-			}
-			
-			foreach($unset as $k) {
-				unset($processes[$k]);
-			}
-			
-			$processes = array_filter($processes);
 		}
 		
-		print('total count ' . $killed);
+		foreach( $stack as $t) {
+			print('starting thread.');
+//			$t->start();
+		}
+	}
+
+	public function openCluster($url)
+	{
+		
+	}
+
+	public function proceedContent($source, $sourceId, $url)
+	{
+		
 	}
 
 }
