6fae0b119f8eb0fec375cc06070919e25dc54432
yergo <yergo.barnett@gmail.com>
2015-06-16 14:56:13 +0200

    silent test controller

--- app/modules/frontend/controllers/TestController.php
+++ app/modules/frontend/controllers/TestController.php
@@ -19,22 +19,22 @@ class TestController extends ControllerBase
 	public function indexAction()
 	{
 		
-		$builder = $this->modelsManager->createBuilder()
-			->addFrom('\Application\Models\Entities\Advertisement', 'A')
-			->leftJoin('\Application\Models\Entities\AdvertisementIgnore', 'A.id = I.advertisement_id', 'I')
-			->where('A.middleman = 0')
-			->andWhere('A.skipped = 0')
-			->andWhere('I.id IS NULL OR I.timeout < NOW()')
-			->orderBy('A.updated ASC')
-		;
-		
-		$advertisements = $builder->getQuery()->execute();
+//		$builder = $this->modelsManager->createBuilder()
+//			->addFrom('\Application\Models\Entities\Advertisement', 'A')
+//			->leftJoin('\Application\Models\Entities\AdvertisementIgnore', 'A.id = I.advertisement_id', 'I')
+//			->where('A.middleman = 0')
+//			->andWhere('A.skipped = 0')
+//			->andWhere('I.id IS NULL OR I.timeout < NOW()')
+//			->orderBy('A.updated ASC')
+//		;
+//		
+//		$advertisements = $builder->getQuery()->execute();
 		
 		$result = [];
 		
-		foreach($advertisements as $advertisement) {
-			$result[$advertisement->getPhone()][] = $advertisement->toArray();
-		}
+//		foreach($advertisements as $advertisement) {
+//			$result[$advertisement->getPhone()][] = $advertisement->toArray();
+//		}
 		
 		$this->view->advertisements = $result;
 		
